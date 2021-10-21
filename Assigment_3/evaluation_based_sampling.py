e8dc6ed0cad22c7eea9500a8368193dc5ff4f041
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-10-20 20:03:14 -0700

    Add assigment 3 raw files, + implementing the Importance sampling

---
+++ Assigment_3/evaluation_based_sampling.py
@@ -0,0 +1,160 @@
+from daphne import daphne
+from tests import is_tol, run_prob_test,load_truth
+
+import torch
+
+from primitives import baseprimitives, distlist
+
+        
+def evaluate_program(ast, sigma={}):
+    """Evaluate a program as desugared by daphne, generate a sample from the prior
+    Args:
+        ast: json FOPPL program
+    Returns: sample from the prior of ast
+    """
+    funcs = {}
+    final_ast = ast
+    if isinstance(ast, list):
+        if isinstance(ast[0], list):
+            if ast[0][0] == 'defn':
+                for statement in ast:
+                    if statement[0]== 'defn':
+                        funcs[statement[1]] = (statement[1], statement[2], statement[3])
+                        final_ast = final_ast[1:]
+                    else:
+                        result, sigma = eval(statement, sigma, {}, funcs)
+            if final_ast[0][0] != 'defn':
+                result, sigma = eval(final_ast[0], sigma, {}, funcs)
+        else:
+            result, sigma = eval(ast, sigma, {}, funcs)
+    else:
+        result, sigma = eval(ast, sigma, {}, funcs)
+    if sigma == {}:
+        results = result
+    else:
+        results = [result, sigma]
+    return results
+
+def eval(x, sigma, l, funcs):
+    "Evaluate an expression in an environment."
+    if isinstance(x, list) and len(x) == 1:
+        x = x[0]
+    if not isinstance(x, list):
+        if isinstance(x, int) or isinstance(x, float):
+            result = torch.tensor(x, dtype=float)
+        elif x in baseprimitives or torch.is_tensor(x) or x in funcs or x in distlist:
+            result = x
+        else:
+            result = l[x]
+    elif x[0] == 'if':
+        cond_result, sigma = eval(x[1], sigma, l, funcs)
+        if cond_result:
+            result = x[2]
+        else:
+            result = x[3]
+    elif x[0] == 'let':
+        result_temp, sigma = eval(x[1][1], sigma, l, funcs)
+        for e in x[2:-1]:
+            _, sigma = eval(e, sigma, {**l, **{x[1][0]: result_temp}}, funcs)
+        result, sigma = eval(x[-1], sigma, {**l, **{x[1][0]: result_temp}}, funcs)
+    elif x[0] == 'sample':
+        dist, sigma = eval(x[1], sigma, l, funcs)
+        result = dist.sample()
+    elif x[0] == 'observe':
+        dist, sigma = eval(x[1], sigma, l, funcs)
+        result, sigma = eval(x[2], sigma, l, funcs)
+        sigma['logW'] = sigma['logW'] + dist.log_prob(result)
+    else:
+        statements = []
+        for expression in x:
+            statement, sigma = eval(expression, sigma, l, funcs)
+            statements.append(statement)
+        
+        first_statemnt, other_statements = statements[0], statements[1:]
+        if first_statemnt in baseprimitives:
+            result = baseprimitives[first_statemnt](other_statements)
+        elif first_statemnt in distlist:
+            result = distlist[first_statemnt](other_statements)
+
+        elif first_statemnt in funcs:
+            _, variables, process = funcs[first_statemnt]
+            assignment = {key:value for key, value in zip(variables, other_statements)}
+            result, sigma = eval(process, sigma, {**l, **assignment}, funcs)
+        else:
+            result = torch.tensor(statements)
+    return result, sigma
+           
+    
+def likelihood_weighting(L, exp):
+    results = torch.zeros(L)
+    weights = torch.zeros(L)
+    for l in range(L):
+        sigma = {'logW':0}
+        results[l] , sigma_temp = evaluate_program(exp, sigma)
+        weights[l] = sigma_temp['logW']
+    return results, weights
+
+def expectation_calculator(results, log_weights, func, *args):
+    weights = torch.exp(log_weights)
+    return torch.sum(weights * func(results, *args)) / torch.sum(weights)
+
+def get_stream(ast):
+    """Return a stream of prior samples"""
+    while True:
+        yield evaluate_program(ast)
+    
+
+
+def run_deterministic_tests():
+    
+    for i in range(1,14):
+        #note: this path should be with respect to the daphne path!
+        ast = daphne(['desugar', '-i', '/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/deterministic/test_{}.daphne'.format(i)])
+        truth = load_truth('/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/deterministic/test_{}.truth'.format(i))
+        ret = evaluate_program(ast)
+        print(ret)
+        try:
+            assert(is_tol(ret, truth))
+        except AssertionError:
+            raise AssertionError('return value {} is not equal to truth {} for exp {}'.format(ret,truth,ast))
+        
+        print('Test passed')
+        
+    print('All deterministic tests passed')
+    
+
+
+def run_probabilistic_tests():
+    
+    num_samples=1e4
+    max_p_value = 1e-4
+    
+    for i in range(1,7):
+        #note: this path should be with respect to the daphne path!        
+        ast = daphne(['desugar', '-i', '/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+        truth = load_truth('/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/probabilistic/test_{}.truth'.format(i))
+        stream = get_stream(ast)
+        p_val = run_prob_test(stream, truth, num_samples)
+        
+        print('p value', p_val)
+        try:
+            assert(p_val > max_p_value)
+        except AssertionError:
+            raise AssertionError('wrong answer')
+        print(f'Test {i} passed')
+
+    print('All probabilistic tests passed')    
+
+        
+if __name__ == '__main__':
+
+    # run_deterministic_tests()
+    # run_probabilistic_tests()
+
+    for i in range(1,2):
+        ast = daphne(['desugar', '-i', '/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/{}.daphne'.format(i)])
+        print('\n\n\nSample of posterior of program {}:'.format(i))
+        results, weights = likelihood_weighting(10000, ast)
+        mean = expectation_calculator(results, weights, lambda x:x)
+        var = expectation_calculator(results, weights, lambda x: x**2 - mean**2)
+        print(mean, var)
---
+++ Assigment_3/graph_based_sampling.py
@@ -0,0 +1,142 @@
+import torch
+import torch.distributions as dist
+
+from daphne import daphne
+
+from primitives import baseprimitives, distlist
+from evaluation_based_sampling import evaluate_program
+
+from tests import is_tol, run_prob_test,load_truth
+
+def topological_sort(graph):
+    nodes = graph[1]['V']
+    edges = graph[1]['A']
+    is_visited = dict.fromkeys(nodes, False)
+    node_stack = []
+    node_order_reverse = []
+    for node in nodes:
+        if not is_visited[node]:
+            node_stack.append((node, False))
+        while len(node_stack) > 0:
+            node, flag = node_stack.pop()
+            if flag:
+                node_order_reverse.append(node)
+                continue
+            is_visited[node] = True
+            node_stack.append((node, True))
+            if node not in edges:
+                continue
+            children = edges[node]
+            for child in children:
+                if not is_visited[child]:
+                    node_stack.append((child, False))
+    return node_order_reverse[::-1]
+
+# Put all function mappings from the deterministic language environment to your
+# Python evaluation context here:
+env = {**baseprimitives, **distlist}
+
+def deterministic_eval(exp):
+    "Evaluation function for the deterministic target language of the graph based representation."
+    if isinstance(exp, list):
+        if exp[0] == 'hash-map':
+            exp = ['hash-map'] + [value for expression in exp[1:] for value in expression]
+    return evaluate_program(exp)
+
+def value_subs(expressions, variables):
+    if isinstance(expressions, list):
+        result = []
+        for expression in expressions:
+            result.append(value_subs(expression, variables))
+    else:
+        if expressions in variables:
+            result = variables[expressions]
+        else:
+            result = expressions
+    return result
+
+def sample_from_joint(graph):
+    "This function does ancestral sampling starting from the prior."
+    node_order = topological_sort(graph)
+    results = {}
+    for node in node_order:
+        first_statement, *other_statements = graph[1]['P'].get(node)
+        if first_statement == 'sample*':
+            dist = deterministic_eval(value_subs(other_statements, results))
+            result = dist.sample()
+        if first_statement == 'observe*':
+            result = graph[1]['Y'].get(node)
+        results[node] = result
+    return deterministic_eval(value_subs(graph[2], results))
+
+
+def get_stream(graph):
+    """Return a stream of prior samples
+    Args: 
+        graph: json graph as loaded by daphne wrapper
+    Returns: a python iterator with an infinite stream of samples
+        """
+    while True:
+        yield sample_from_joint(graph)
+
+
+
+
+#Testing:
+
+def run_deterministic_tests():
+    
+    for i in range(1,13):
+        #note: this path should be with respect to the daphne path!
+        graph = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/deterministic/test_{}.daphne'.format(i)])
+        truth = load_truth('/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/deterministic/test_{}.truth'.format(i))
+        ret = deterministic_eval(graph[-1])
+        print(ret)
+        try:
+            assert(is_tol(ret, truth))
+        except AssertionError:
+            raise AssertionError('return value {} is not equal to truth {} for graph {}'.format(ret,truth,graph))
+        
+        print('Test passed')
+        
+    print('All deterministic tests passed')
+    
+
+
+def run_probabilistic_tests():
+    
+    #TODO: 
+    num_samples=1e4
+    max_p_value = 1e-4
+    
+    for i in range(1,7):
+        #note: this path should be with respect to the daphne path!        
+        graph = daphne(['graph', '-i', '/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+
+        truth = load_truth('/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2/programs/tests/probabilistic/test_{}.truth'.format(i))
+        
+        stream = get_stream(graph)
+        
+        p_val = run_prob_test(stream, truth, num_samples)
+        
+        print('p value', p_val)
+        assert(p_val > max_p_value)
+    
+    print('All probabilistic tests passed')    
+
+
+        
+        
+if __name__ == '__main__':
+    
+
+    run_deterministic_tests()
+    run_probabilistic_tests()
+
+
+    for i in range(1,5):
+        graph = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_2//programs/{}.daphne'.format(i)])
+        print('\n\n\nSample of prior of program {}:'.format(i))
+        print(sample_from_joint(graph))    
+
+    

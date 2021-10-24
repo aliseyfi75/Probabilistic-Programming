7e60ea271411ac03280057e3c1f9b040b809b310
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-10-24 13:03:32 -0700

    add trace plots and modify labels

--- Assignment_3/plot.py
+++ Assignment_3/plot.py
@@ -5,18 +5,18 @@ import seaborn as sns
 sns.set_style("white")
 dirn = os.path.dirname(os.path.abspath(__file__))
 # labels = { 1: "", 2: "", 3: "", 4: {0: "W_0", 1: "b_0", 2: "W_1", 3: "b_1" }}
-labels = {1:{0:""}, 2:{0:'bias', 1:'slope'}, 3:{0:""}, 4:{0:""}, 5:{0:""}}
-def get_title(eval,i,dim=False,d=0):
+labels = {1:{0:"mu"}, 2:{0:'slope', 1:'bias'}, 3:{0:"equal_clusters"}, 4:{0:"is_raining"}, 5:{0:"x", 1:"y"}}
+def get_title(eval,i,dim=False,d=0, fig_type='hist'):
     if dim:
         label = labels[i][d]
-        return "{} Sampled Distribution for Program {} {}".format(eval, i, label)
+        return "{} Sampled {} for Program {} {}".format(eval, fig_type, i, label)
     else:
-        return "{} Sampled Distribution for Program {}".format(eval, i)
-def get_fname(eval,i,dim=False,d=0):
+        return "{} Sampled {} for Program {}".format(eval,fig_type, i)
+def get_fname(eval,i,dim=False,d=0, fig_type='hist'):
     if dim:
-        return dirn + '/figures/{}_plt_program_{}_d_{}.jpg'.format(eval,i,d)
+        return dirn + '/figures/{}_plt_{}_program_{}_d_{}.jpg'.format(eval,fig_type, i,d)
     else:
-        return dirn + '/figures/{}_plt_program_{}.jpg'.format(eval,i)
+        return dirn + '/figures/{}_plt_{}_program_{}.jpg'.format(eval,fig_type, i)
 def plot_hists_n(eval, i, hists):
     for d in range(len(hists)):
         samples = np.array(hists[d])
@@ -35,6 +35,25 @@ def plot_hists_n(eval, i, hists):
             fig.suptitle(title, fontsize=24)
             plt.savefig(fname)
             plt.clf()
+
+def plot_traces_n(eval, i, traces):
+    for d in range(len(traces)):
+        samples = np.array(traces[d])
+        if samples.ndim < 3:
+            # 2D >= random var
+            plot_trace_arr(eval, i, samples,dim=True,d=d)
+        else:
+            # ND random var 
+            _,k,_ = samples.shape 
+            fig, axs = plt.subplots(2,5, figsize=(20,10), dpi=100)
+            for j, ax in zip(range(k), axs.flat):
+                sns.lineplot(data=samples[:][j],ax=ax)
+                ax.title.set_text('range over {}[{}]'.format(labels[i][d],j))
+            fname = get_fname(eval, i,True,d, fig_type='trace')
+            title = get_title(eval, i, True, d, fig_type='trace')
+            fig.suptitle(title, fontsize=24)
+            plt.savefig(fname)
+            plt.clf()
 def plot_hist_arr(eval, i, samples, dim=False, d=0):
     plt.figure(figsize=(10,7))
     nbins = int(max(samples.max()-samples.min(),30))
@@ -44,6 +63,16 @@ def plot_hist_arr(eval, i, samples, dim=False, d=0):
     plt.title(title)
     plt.savefig(fname)
     plt.clf()
+
+def plot_trace_arr(eval, i, samples, dim=False, d=0):
+    plt.figure(figsize=(10,7))
+    plt.plot(samples)
+    fname = get_fname(eval, i,dim,d, fig_type='trace')
+    title = get_title(eval, i, dim, d, fig_type='trace')
+    plt.title(title)
+    plt.savefig(fname)
+    plt.clf()
+
 def plot_hist(eval, i, samples, bins):
     plt.figure(figsize=(10,7))
     sns.histplot(data=samples,bins=bins,kde=True, stat='density')
@@ -52,6 +81,16 @@ def plot_hist(eval, i, samples, bins):
     plt.title(title)
     plt.savefig(fname)
     plt.clf()
+
+def plot_trace(eval, i, samples):
+    plt.figure(figsize=(10,7))
+    plt.plot(samples)
+    fname = get_fname(eval, i, fig_type='trace')
+    title = get_title(eval, i, fig_type='trace')
+    plt.title(title)
+    plt.savefig(fname)
+    plt.clf()
+
 def gen_hists(eval, i, samples):
     try:
         # list of c
@@ -72,5 +111,30 @@ def gen_hists(eval, i, samples):
             for d in range(samples.shape[0]):
                 hists.append(samples[d,:].T)
             plot_hists_n(eval, i, hists)
+
+def gen_traces(eval, i, samples):
+    try:
+        # list of c
+        min_v, max_v = min(samples), max(samples)
+        nbins = int(min(max_v-min_v,30))
+        bins = np.linspace(min_v, max_v,nbins)
+        plot_trace(eval, i, samples, bins)
+    except:
+        try:
+            # 1 rnd var
+            assert(samples.dim <= 1)
+            samples = samples.detach().numpy().T
+            plot_trace_arr(eval, i, samples)
+        except:
+            # n rnd vars
+            traces = []
+            samples = samples.detach().numpy()
+            for d in range(samples.shape[0]):
+                traces.append(samples[d,:].T)
+            plot_traces_n(eval, i, traces)
+
 def draw_hists(eval, samples, i):
     gen_hists(eval, i, samples)
+
+def draw_trace(eval, samples, i):
+    gen_traces(eval, i, samples)

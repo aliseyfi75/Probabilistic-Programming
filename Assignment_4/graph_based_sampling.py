2d814e2ef973ca0029379e6c4afc8de9ac80ca54
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-11-02 19:10:24 -0700

    Initial version of the code, need some works...

--- Assignment_4/graph_based_sampling.py
+++ Assignment_4/graph_based_sampling.py
@@ -2,6 +2,7 @@ import torch
 
 import copy
 import time
+import numpy as np
 
 from daphne import daphne
 
@@ -10,7 +11,12 @@ from evaluation_based_sampling import evaluate_program, expectation_calculator
 
 from tests import is_tol, run_prob_test,load_truth
 
-from plot import draw_hists, draw_trace, draw_log_joint
+from plot import draw_hists, draw_trace, draw_log_joint, draw_hitmap
+import matplotlib.pyplot as plt
+
+# import wandb
+
+# wandb.init(project="HW4", entity="aliseyfi")
 
 def topological_sort(graph):
     nodes = graph[1]['V']
@@ -256,6 +262,161 @@ def grad_energy(P, unobserved_variables, observed_variables):
         U_gradients[i] = unobserved_variables[key].grad
     return U_gradients
 
+
+def BBVI_evaluator(order_node, graph, sigma):
+    P = graph[1]['P']
+    Y = graph[1]['Y']
+    Q = sigma['Q']
+    G = sigma['G']
+    optimizer = sigma['optimizer']
+    results = {}
+
+    for node in order_node:
+        link_function = P[node][0]
+
+        if link_function == 'sample*':
+            d = deterministic_eval(value_subs(P[node][1], results))
+            if node not in Q:
+                Q[node] = d.make_copy_with_grads()
+                optimizer[node] = torch.optim.Adam(Q[node].parameters(), lr=0.01)
+            result = Q[node].sample()
+            G[node] = grad_log_prob(Q[node], result)
+            try:
+                sigma_temp = d.log_prob(result) - Q[node].log_prob(result)
+                sigma['logW'] += sigma_temp
+            except:
+                sigma['logW'] += 0
+        
+        elif link_function == 'observe*':
+            result = torch.tensor(Y[node])
+            d = deterministic_eval(value_subs(P[node][1], results))
+            sigma_temp = d.log_prob(result)
+            sigma['logW'] += sigma_temp
+        
+        results[node] = result
+    
+    return results, sigma
+
+def grad_log_prob(dist, value):
+    log_prob = dist.log_prob(value)
+    log_prob.backward()
+    #.clone().detach()
+    grad = [param.grad for param in dist.parameters()]
+    return grad
+
+def BBVI(graph, T, L):
+    sigma = {'Q':{}, 'optimizer':{}}
+    order_node = topological_sort(graph)
+    
+    results = []
+    log_weights = []
+    posteriers = []
+
+    for t in range(T):
+        sigma['G'] = {}
+        gradients = []
+        log_ws = []
+
+        for l in range(L):
+            sigma['logW'] = 0
+            result, sigma = BBVI_evaluator(order_node, graph, sigma)
+            gradients.append(copy.deepcopy(sigma['G']))
+            log_ws.append(sigma['logW'])
+
+        if t==0:
+            posteriers.append(copy.deepcopy(sigma['Q']['sample2'].parameters()))
+        
+        ELBO_gradients(gradients, log_ws, sigma['Q'])
+
+        for optimizer in sigma['optimizer'].values():
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        post_temp = {}
+        for q in sigma['Q']:
+            post_temp[q] = sigma['Q'][q].parameters().copy()
+
+        posteriers.append(post_temp)
+        result_temp = deterministic_eval(value_subs(graph[2], result))
+        results.append(result_temp)
+        log_weights.append(log_ws[-1])
+
+        # print_weights = torch.exp(torch.stack(log_weights)).detach().numpy()
+        # print_results = torch.stack(results).detach().numpy()
+
+        # print_mean = (print_results * print_weights.reshape(-1,1)).sum(axis=0) / print_weights.sum()
+        
+        # wandb.log({'ELBO': torch.mean(torch.stack(log_weights)).detach().numpy()})
+
+        # for i in range(len(print_mean)):
+        #     wandb.log({'mean_'+str(i): print_mean[i]})
+
+
+    return results, log_weights, posteriers
+
+def inf_skipper(gradients, log_ws):
+    temp_gradients = []
+    temp_log_ws = []
+
+    for i in range(len(log_ws)):
+        if log_ws[i] == float('-inf'):
+            continue
+        temp_gradients.append(gradients[i])
+        temp_log_ws.append(log_ws[i])
+    
+    return temp_gradients, temp_log_ws
+
+def ELBO_gradients(gradients, log_ws, posteriors):
+    
+    gradients, log_ws = inf_skipper(gradients, log_ws)
+    len_grads = len(gradients)
+
+    var_union = list(set([var for grad in gradients for var in grad]))
+    
+    Fs = []
+    Gs = []
+    stack = {}
+
+    for var in var_union:
+        gradient_var = gradients[0][var]
+        if len(gradient_var[0].shape) > 0 and len(gradient_var[0]) > 1:
+            gradient_var = [grad.clone().detach().requires_grad_(True) for grad in gradient_var[0]]
+            stack[var] = len(gradient_var)
+
+        len_vars = len(gradient_var)
+
+        G_var = torch.zeros((len_grads, len_vars))
+        F_var = torch.zeros((len_grads, len_vars))
+
+        for lg in range(len_grads):
+            G_var[lg, :] = torch.stack(gradients[lg][var])
+            F_var[lg, :] = G_var[lg, :] * log_ws[lg]
+        Gs.append(G_var.detach().numpy())
+        Fs.append(F_var.detach().numpy())
+    
+    Gs = np.column_stack(Gs)
+    Fs = np.column_stack(Fs)
+    
+    num = np.sum([np.cov(Fs[:, v], Gs[:, v])[0, 1] for v in range(Gs.shape[1])])
+    denum = np.sum([np.var(Gs[:, v]) for v in range(Gs.shape[1])])
+    b_hat = 0.
+    if not denum == 0. and not np.isnan(num):
+        b_hat = num/denum
+
+    counter_1 = 0
+    for var in var_union:
+        gradient_var = gradients[0][var]
+        counter_2 = len(gradient_var)
+        if var in stack:
+            counter_2 = stack[var]
+        g_hat = np.array([np.sum(Fs[:, v] - b_hat * Gs[:, v]) / len_grads for v in range(counter_1, counter_1+counter_2)])
+        # g_hat = np.array([np.sum(Fs[:, v]) / len_grads for v in range(counter_1, counter_1 + counter_2)])
+        if var in stack:
+            g_hat = [g_hat]
+        for i, parameter in enumerate(posteriors[var].parameters()):
+            parameter.grad = torch.tensor(-g_hat[i], dtype=parameter.grad.dtype)
+        counter_1 += counter_2
+    return
 def get_stream(graph):
     """Return a stream of prior samples
     Args: 
@@ -317,63 +478,101 @@ if __name__ == '__main__':
     # run_deterministic_tests()
     # run_probabilistic_tests()
 
+    # Task 1
+    # graph_1 = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_4/programs/{}.daphne'.format(1)])
+    # print('\n\n\nSample of posterior of program {}:'.format(1)) 
+    # T_1 = int(5 * 1e3)
+    # L_1 = 25
+    # start_time_1 = time.time()
+    # samples_1, log_weights_1, posteriors_1 = BBVI(graph_1, T_1, L_1)
+    # print('Time taken:', time.time() - start_time_1)
+    # samples_1 = torch.stack(samples_1).numpy()
+    # weights_1 = np.exp(torch.stack(log_weights_1).detach().numpy())
+    
+    # samples_mean = (samples_1 * weights_1).sum() / weights_1.sum()
+    # samples_var =  ((samples_1 **2 - samples_mean ** 2) * weights_1).sum() / weights_1.sum()
+    
+    # print(samples_mean)
+    # print(samples_var)
 
-    for i in range(1,6):
-        graph = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_3//programs/{}.daphne'.format(i)])
-        print('\n\n\nSample of posterior of program {}:'.format(i)) 
-        # MH within gibbs
-        n_samples = int(1e5)
-        print('\n\n\nMH within Gibbs:')
-        start_time = time.time()
-        samples, nodes_values = mh_within_gibbs_sampling(graph, num_samples=n_samples)
-        print('Time taken: {}'.format(time.time() - start_time))
-
-        samples_mean = expectation_calculator(samples, torch.zeros(samples.shape[1]), lambda x:x)
-        samples_var = expectation_calculator(samples, torch.zeros(samples.shape[1]), lambda x: x**2 - samples_mean.view(samples.shape[0],1)**2)
-
-        print('number of samples: ', n_samples)
-        print('Mean:', samples_mean)
-        print('Var:', samples_var)
-
-        draw_hists("MH_within_Gibbs", samples, i)
-
-        if i<3 or i==5:
-            draw_trace("MH_within_Gibbs", samples, i)
-            draw_log_joint("MH_within_Gibbs", i, graph, nodes_values, deterministic_eval, value_subs)
+    # new_samples = torch.distributions.Normal(*posteriors_1[-1]['sample2']).sample((10000,)).view(1,-1)
+    # draw_hists('BBVI', new_samples, 1)
 
 
-        if i == 2:
-            # Computing covariance matrix of samples using matrix multiplication
-            samples_rm_mean = samples.T - samples_mean
-            cov_matrix = torch.matmul(torch.t(samples_rm_mean), samples_rm_mean) / (samples_rm_mean.shape[0] - 1)
-            print('Covariance matrix:')
-            print(cov_matrix)
+    # Task 2
+    # graph_2 = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_4/programs/{}.daphne'.format(2)])
+    # print('\n\n\nSample of posterior of program {}:'.format(2)) 
+    # T_2 = int(5 * 1e3)
+    # L_2 = 25
+    # start_time_2 = time.time()
+    # samples_2, log_weights_2, posteriors_2 = BBVI(graph_2, T_2, L_2)
+    # print('Time taken:', time.time() - start_time_2)
+    # samples_2 = torch.stack(samples_2).numpy()
+    # weights_2 = np.exp(torch.stack(log_weights_2).detach().numpy())
+    
+    # samples_mean_slope = (samples_2[:,0] * weights_2).sum() / weights_2.sum()
 
-        # HMC
-        if i<3 or i==5:
-            print('\n\n\nHMC:')
-            n_samples = int(1e4)
-            start_time = time.time()
-            samples, nodes_values = hmc(graph, num_samples=n_samples)
-            print('Time taken: {}'.format(time.time() - start_time))
+    # samples_mean_bias = (samples_2[:,1] * weights_2).sum() / weights_2.sum()
+    
+    # print(samples_mean_slope)
+    # print(samples_mean_bias)
 
-            samples_mean = expectation_calculator(samples, torch.zeros(samples.shape[1]), lambda x:x)
-            samples_var = expectation_calculator(samples, torch.zeros(samples.shape[1]), lambda x: x**2 - samples_mean.view(samples.shape[0],1)**2)
+    # Task 3
+    # graph_3 = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_4/programs/{}.daphne'.format(3)])
+    # print('\n\n\nSample of posterior of program {}:'.format(3)) 
+    # T_3 = int(5 * 1e1)
+    # L_3 = 25
+    # start_time_3 = time.time()
+    # samples_3, log_weights_3, posteriors_3 = BBVI(graph_3, T_3, L_3)
+    # print('Time taken:', time.time() - start_time_3)
+    # samples_3 = torch.stack(samples_3).numpy()
+    # weights_3 = np.exp(torch.stack(log_weights_3).detach().numpy())
+    
+    # samples_mean = (samples_3 * weights_3.reshape(-1,1)).sum(axis=0) / weights_3.sum()
+    
+    # print(samples_mean)
 
-            print('number of samples: ', n_samples)
-            print('Mean:', samples_mean)
-            print('Var:', samples_var)
+    # Task 4
+    # graph_4 = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_4/programs/{}.daphne'.format(4)])
+    # print('\n\n\nSample of posterior of program {}:'.format(4)) 
+    # T_4 = int(5 * 1e0)
+    # L_4 = 25
+    # start_time_4 = time.time()
+    # samples_4, log_weights_4, posteriors_4 = BBVI(graph_4, T_4, L_4)
+    # print('Time taken:', time.time() - start_time_4)
+    # w_0 = np.zeros((10,1))
+    # b_0 = np.zeros((10,1))
+    # w_1 = np.zeros((10,10))
+    # b_1 = np.zeros((10,1))
 
-            if i == 2:
-                samples_rm_mean = samples.T - samples_mean
-                cov_matrix = torch.matmul(torch.t(samples_rm_mean), samples_rm_mean) / (samples_rm_mean.shape[0] - 1)
-                print('Covariance matrix:')
-                print(cov_matrix)
+    # weights = np.exp(torch.stack(log_weights_4).detach().numpy())
+    # for i, sample in enumerate(samples_4):
+    #     w_0 += sample[0].numpy() * weights[i]
+    #     b_0 += sample[1].numpy() * weights[i]
+    #     w_1 += sample[2].numpy() * weights[i]
+    #     b_1 += sample[3].numpy() * weights[i]
 
-            draw_hists("HMC", samples, i)
+    # sum_weights = weights.sum()
 
-            draw_trace("HMC", samples, i)
+    # w_0 /= sum_weights
+    # b_0 /= sum_weights
+    # w_1 /= sum_weights
+    # b_1 /= sum_weights
 
-            draw_log_joint("HMC", i, graph, nodes_values, deterministic_eval, value_subs)
+    # draw_hitmap('BBVI', w_0, '4_w0')
+    # draw_hitmap('BBVI', b_0, '4_b0')
+    # draw_hitmap('BBVI', w_1, '4_w1')
+    # draw_hitmap('BBVI', b_1, '4_b1')
 
-            
+
+    # Task 5
+    graph_5 = daphne(['graph','-i','/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_4/programs/{}.daphne'.format(5)])
+    print('\n\n\nSample of posterior of program {}:'.format(5))
+    T_5 = int(5 * 1e3)
+    L_5 = 25
+    start_time_5 = time.time()
+    samples_5, log_weights_5, posteriors_5 = BBVI(graph_5, T_5, L_5)
+    print('Time taken:', time.time() - start_time_5)
+
+    new_samples = torch.distributions.Uniform(*posteriors_5[-1]['sample2']).sample((10000,)).view(1,-1)
+    draw_hists('BBVI', new_samples, 5)

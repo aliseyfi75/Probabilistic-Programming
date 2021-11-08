234712170a4deb6b69946c5b5c602caa3abca44a
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-11-07 17:46:48 -0800

    Add initial files

---
+++ Assignment_5/daphne.py
@@ -0,0 +1,10 @@
+import json
+import subprocess
+
+def daphne(args, cwd='/Users/aliseyfi/Documents/UBC/Semester3/Probabilistic-Programming/HW/Probabilistic-Programming/Assignment_5/daphne'):
+    proc = subprocess.run(['lein','run'] + args,
+                          capture_output=True, cwd=cwd)
+    if(proc.returncode != 0):
+        raise Exception(proc.stdout.decode() + proc.stderr.decode())
+    return json.loads(proc.stdout)
+

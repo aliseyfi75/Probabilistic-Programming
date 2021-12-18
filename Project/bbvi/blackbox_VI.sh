68409b3a1a46ad959d9dc63bd0c4b2e9fb499f86
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-12-17 16:26:57 -0800

    Change bbvi bash file

--- Project/bbvi/blackbox_VI.sh
+++ Project/bbvi/blackbox_VI.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --array=5,10,15,20
-#SBATCH --job-name=BBVI_005
-#SBATCH --account=def-condon
-#SBATCH --mail-user=jlovrod@cs.ubc.ca 
+#SBATCH --job-name=BBVI
+#SBATCH --account=def-rngubc
+#SBATCH --mail-user=ali.seyfi.12@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=4000
+#SBATCH --ntasks=8
+#SBATCH --mem=2000
 #SBATCH --time=10:00:00
 
 module load python/3.7
@@ -15,4 +14,4 @@ source /home/jlovrod/projects/def-condon/jlovrod/dp_gen_env/bin/activate
 
 wandb login 33f5ffa304d256a59512bb634dcf8da21304837f
 
-python blackbox_VI.py --L $SLURM_ARRAY_TASK_ID --lr 0.005
+python -u blackbox_VI.py --L $SLURM_ARRAY_TASK_ID --lr 0.005 > bbvi.out

95d1b404eee35c731ada08120c8460128bbf21ff
Ali Seyfi <ali.seyfi.12@gmail.com>
2021-12-22 10:40:56 -0800

    SMC new results added

--- Project/smc_IS/Experiment_with_BBVI/run.sh
+++ Project/smc_IS/Experiment_with_BBVI/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=100,250,500,750,1000,2500,5000,7500
+#SBATCH --array=10,30,50,70,90,110,200,300,400
 #SBATCH --job-name=smc_without
 #SBATCH --account=def-rngubc
 #SBATCH --mail-user=ali.seyfi.12@gmail.com
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=8
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4000
-#SBATCH --time=10:00:00
+#SBATCH --time=03:00:00
 
 module load python/3.7
 source $HOME/pytorch/bin/activate
